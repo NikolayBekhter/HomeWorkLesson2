f5c1fbe3f9656970f24f1a8380a3e31d9188805c
NikolayBekhter <n.v.bekhter@mail.ru>
2022-01-14 00:10:03 +0300

    written CheckingSum

--- src/main/java/ru/geekbrains/lesson2/HomeWorkLesson2.java
+++ src/main/java/ru/geekbrains/lesson2/HomeWorkLesson2.java
@@ -3,10 +3,11 @@ package ru.geekbrains.lesson2;
 public class HomeWorkLesson2 {
 
     public static void main(String[] args) {
-        LeapYearOrNot(2022);
+        System.out.println(LeapYearOrNot(2022));
+        System.out.println(CheckingSum(7, 13));
     }
 
-    public static void LeapYearOrNot(int year) {
+    public static boolean LeapYearOrNot(int year) {
         boolean result = false;
         if(year % 400 == 0){
             result = true;
@@ -17,6 +18,17 @@ public class HomeWorkLesson2 {
         } else{
             result = false;
         }
-        System.out.println(result);
+        return result;
+    }
+
+    public static boolean CheckingSum(int a, int b) {
+        boolean result = false;
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
     }
 }
