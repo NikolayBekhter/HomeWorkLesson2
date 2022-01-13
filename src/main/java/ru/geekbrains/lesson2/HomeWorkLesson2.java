ec44699d41ff1d61e4efb949623886fcbda8026c
NikolayBekhter <n.v.bekhter@mail.ru>
2022-01-13 23:41:10 +0300

    written LeapYearOrNot

--- src/main/java/ru/geekbrains/lesson2/HomeWorkLesson2.java
+++ src/main/java/ru/geekbrains/lesson2/HomeWorkLesson2.java
@@ -2,4 +2,21 @@ package ru.geekbrains.lesson2;
 
 public class HomeWorkLesson2 {
 
+    public static void main(String[] args) {
+        LeapYearOrNot(2022);
+    }
+
+    public static void LeapYearOrNot(int year) {
+        boolean result = false;
+        if(year % 400 == 0){
+            result = true;
+        } else if(year % 100 == 0){
+            result = false;
+        } else if(year % 4 == 0){
+            result = true;
+        } else{
+            result = false;
+        }
+        System.out.println(result);
+    }
 }
